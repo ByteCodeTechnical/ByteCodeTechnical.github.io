6ee04709033523ee23d03a2712e5dd78d8cd60df
ByteCodeTechnical <bytetech582@gmail.com>
2025-09-29 21:44:55 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,23 @@
-// nav menu style
 var nav = $("#navbarSupportedContent");
 var btn = $(".custom_menu-btn");
-btn.click
-btn.click(function (e) {
 
+btn.click(function (e) {
     e.preventDefault();
     nav.toggleClass("lg_nav-toggle");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 });
 
+// Close menu when clicking outside on small screens
+$(document).click(function(event) {
+    if (!$(event.target).closest('.custom_nav-container').length && nav.hasClass('lg_nav-toggle')) {
+        nav.removeClass('lg_nav-toggle');
+        document.querySelector(".custom_menu-btn").classList.remove("menu_btn-style");
+    }
+});
 
 function getCurrentYear() {
     var d = new Date();
-    var currentYear = d.getFullYear()
-
+    var currentYear = d.getFullYear();
     $("#displayDate").html(currentYear);
 }
 
